440f2f0adce011eabdd8d9c39db1923e0ccd7709
Matthew Wilson <wilsonmatt219@gmail.com>
2019-03-05 20:00:03 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,8 +5,7 @@ require 'prime'
 def prime?(num)
   num < 2 
   return false
-    else
-        (2..Math.sqrt(n).round).each{|x| return false if n % x == 0}
+    else (2..Math.sqrt(n).round).each{|x| return false if n % x == 0}
     end
     true
   
